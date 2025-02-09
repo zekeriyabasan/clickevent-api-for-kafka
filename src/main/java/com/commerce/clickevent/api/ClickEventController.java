2cc8db35a5dd5c9d5e038eb5091dea14f56400d5
zekeriyabasan <zekeriyabasan@gmail.com>
2025-02-09 20:00:03 +0300

    added controller

---
+++ src/main/java/com/commerce/clickevent/api/ClickEventController.java
@@ -0,0 +1,23 @@
+package com.commerce.clickevent.api;
+
+import com.commerce.clickevent.model.ClickEventRequest;
+import com.commerce.clickevent.service.ProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClickEventController {
+
+    @Autowired
+    @Qualifier("click")
+    ProducerService producerService;
+
+    @PostMapping
+    ClickEventRequest clickEventRequest(@RequestBody ClickEventRequest request){
+        ClickEventRequest response = producerService.producer(request);
+        return  response;
+    }
+}
