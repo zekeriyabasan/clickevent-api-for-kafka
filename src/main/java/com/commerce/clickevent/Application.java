5d858e0d0fec908c208062c29b4355122189d9b4
zekeriyabasan <zekeriyabasan@gmail.com>
2025-02-09 19:59:36 +0300

    edit

--- src/main/java/com/commerce/clickevent/Application.java
+++ src/main/java/com/commerce/clickevent/Application.java
@@ -1,10 +1,11 @@
 package com.commerce.clickevent;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-
+        SpringApplication.run(Application.class,args);
     }
 }
--- src/main/java/com/commerce/clickevent/service/ProducerServiceImpl.java
+++ src/main/java/com/commerce/clickevent/service/ProducerServiceImpl.java
@@ -1,14 +1,20 @@
 package com.commerce.clickevent.service;
 
 import com.commerce.clickevent.model.ClickEventRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 @Qualifier("click")
 public class ProducerServiceImpl implements ProducerService{
+
+    @Autowired
+    KafkaCustomProducer kafkaCustomProducer;
+
     @Override
     public ClickEventRequest producer(ClickEventRequest request) {
-
+        kafkaCustomProducer.send(request);
+        return request;
     }
 }
